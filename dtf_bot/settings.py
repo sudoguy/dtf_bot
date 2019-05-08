7772afb235f60323574cd547816c5fe7fb581508
Evgeny <eskemerov@gmail.com>
2019-05-09 01:23:04 +0300

    Remove "secret" key

--- dtf_bot/settings.py
+++ dtf_bot/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "3c-_-=5h1&601w2mvp)@n9fy0bhjrb0#i*@&!1sn8lifp2v8f2"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
