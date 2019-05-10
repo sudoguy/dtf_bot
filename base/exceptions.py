cddb8240ef67433af81b2dd2f688edafa32ea66b
Evgeny <eskemerov@gmail.com>
2019-05-10 22:35:27 +0300

    Add some custom exceptions

--- base/exceptions.py
+++ base/exceptions.py
@@ -1,1 +1,13 @@
 from rest_framework.exceptions import APIException
+
+
+class EntryNotFound(APIException):
+    status_code = 400
+    default_detail = "Entry not found"
+    default_code = "entry_not_found"
+
+
+class InternalServiceError(APIException):
+    status_code = 500
+    default_detail = "Internal error"
+    default_code = "internal_error"
