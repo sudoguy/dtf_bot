6a487912807144ad5c75d98d0ddcd4fd78350c1f
Evgeny Kemerov <eskemerov@gmail.com>
2019-05-24 18:11:09 +0300

    Create when entry doesn't exists

--- base/views.py
+++ base/views.py
@@ -1,14 +1,21 @@
-from rest_framework.views import APIView
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from base.models import Comment, Entry
+from base.tasks import update_entry
 
 
 class NewComment(APIView):
     def post(self, request):
         data = request.data["data"]
+        entry_id = data["content"]["id"]
 
-        entry = Entry.objects.get(data["content"]["id"])
+        try:
+            entry = Entry.objects.get(id=entry_id)
+        except ObjectDoesNotExist:
+            update_entry(entry_id)
+            entry = Entry.objects.get(id=entry_id)
 
         new_comment = Comment(
             id=data["id"],
