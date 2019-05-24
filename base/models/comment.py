6fe5b913ccd2939edab2563e31af17bc7b999a11
Evgeny <eskemerov@gmail.com>
2019-05-24 09:33:59 +0300

    Update Comment model

--- base/models/comment.py
+++ base/models/comment.py
@@ -5,20 +5,21 @@ from django.contrib.postgres.fields import JSONField
 
 
 OTHER, ANDROID, IOS = range(3)
-SOURCE_CHOICES = ((OTHER, "Other"), (ANDROID, "Android"), (IOS, "iOS"))
+UNKNOWN = None
+SOURCE_CHOICES = ((OTHER, "Other"), (ANDROID, "Android"), (IOS, "iOS"), (UNKNOWN, "Unknown"))
 
 
 class Comment(BaseModel):
     id = models.BigIntegerField(primary_key=True)
     text = models.TextField()
-    reply_to = models.IntegerField()
+    reply_to = models.IntegerField(null=True)
 
-    is_favorited = models.BooleanField()
-    is_pinned = models.BooleanField()
-    is_edited = models.BooleanField()
+    is_favorited = models.BooleanField(default=False)
+    is_pinned = models.BooleanField(default=False)
+    is_edited = models.BooleanField(default=False)
 
     level = models.SmallIntegerField()
-    source_id = models.SmallIntegerField(choices=SOURCE_CHOICES)
+    source_id = models.SmallIntegerField(choices=SOURCE_CHOICES, null=True, default=UNKNOWN)
     last_response = JSONField()
 
     entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
