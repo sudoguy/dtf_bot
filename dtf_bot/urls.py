7c2e56333ff01fa639e8ef8e4e50936e668e57aa
Evgeny <eskemerov@gmail.com>
2019-05-24 09:37:31 +0300

    Add url to new comment

--- dtf_bot/urls.py
+++ dtf_bot/urls.py
@@ -16,4 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [path("admin/", admin.site.urls)]
+from base.views import NewComment
+
+urlpatterns = [path("admin/", admin.site.urls), path("new_comment", NewComment.as_view())]
