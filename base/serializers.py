c025959e71b88ce1a6bac90001ca3ce1a138e337
Evgeny <eskemerov@gmail.com>
2019-05-24 09:37:56 +0300

    Started work on serializers

--- base/serializers.py
+++ base/serializers.py
@@ -1,1 +1,34 @@
-from rest_framework.serializers import Serializer
+from rest_framework.serializers import (
+    Serializer,
+    CharField,
+    ChoiceField,
+    IntegerField,
+    JSONField,
+    URLField,
+    UUIDField,
+)
+
+
+class ImageSerializer(Serializer):
+    uuid = UUIDField()
+    width = IntegerField()
+    height = IntegerField()
+    size = IntegerField()
+    type = CharField()
+    color = CharField()
+
+
+IMAGE = "image"
+VIDEO = "video"
+MEDIA_CHOICES = ((IMAGE, "Image"), (VIDEO, "Video"))
+
+
+class MediaSerializer(Serializer):
+    type = ChoiceField(choices=MEDIA_CHOICES)
+
+
+class CommentSerializer(Serializer):
+    id = IntegerField()
+    url = URLField()
+    text = CharField()
+    author = JSONField()
