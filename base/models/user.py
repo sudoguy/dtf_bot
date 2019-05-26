68d4a76b35a02b49239ef1034a69db78c3e3dc24
Evgeny <eskemerov@gmail.com>
2019-05-26 23:50:57 +0300

    Add simple user model

---
+++ base/models/user.py
@@ -0,0 +1,16 @@
+from .base import BaseModel
+from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+
+class User(BaseModel):
+    id = models.BigIntegerField(primary_key=True)
+    created = models.IntegerField(null=True)
+    name = models.CharField(max_length=255)
+
+    url = models.TextField()
+    avatar_url = models.CharField(max_length=255, null=True)
+
+    karma = models.IntegerField()
+    user_hash = models.CharField(max_length=255)
+    last_response = JSONField()
